73d16d22f0320d6520b4e6b13acfd7d8d1a7d423
刘家辉 <liujiahiu@qq.com>
2019-06-24 18:47:03 +0800

    增加VS2017的快捷键

---
+++ VS2017的快捷键.c
@@ -0,0 +1,12 @@
+VS2017的一些快捷键.c
+
+//ctrl + x为删除该行
+//
+//ctrl + K,ctrl + G为显示结构
+//ctrl + K,ctrl + M为显示行注释
+//ctrl + K,ctrl + 左箭头为移到上一注释
+//ctrl + K,ctrl + 右箭头为移到下一注释
+//
+//crtl + -为向后导航
+//crtl + shift + -为向前导航
+//
